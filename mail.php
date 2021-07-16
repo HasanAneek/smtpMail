9c884232ad1df1a82158e0d0c6051c971b1f69f3
JahidAneek <82103572+HasanAneek@users.noreply.github.com>
2021-07-16 23:09:06 +0600

    Update mail.php

--- mail.php
+++ mail.php
@@ -14,16 +14,16 @@ $mail->Host = "smtp.gmail.com";
 $mail->SMTPAuth = "true";
 $mail->SMTPSecure = "tls";
 $mail->Port = "587";
-$mail->Username = "hasan.onic162@gmail.com";
-$mail->Password = "Nsu1620975042";
+$mail->Username = "ab@gmail.com";
+$mail->Password = "123";
 $mail->Subject  = "Smtp Test subject";
-$mail->setFrom("aneek.ndc5085@gmail.com");
+$mail->setFrom("cd@gmail.com");
 $mail->isHTML(true);
 $mail->addAttachment("file\Covid_vaccination.pdf");
 $mail->addAttachment("file\Me.jpg");
 $mail->Body = "<h2>This is Html text</h2><br>
 <p>This is Html paragraph demo</p> ";
-$mail->addAddress("aneek.ndc5085@gmail.com");
+$mail->addAddress("cd@gmail.com");
 if($mail->send()){
     echo "Successful";
 }else{
